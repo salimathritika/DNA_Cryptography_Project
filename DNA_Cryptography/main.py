1668bd1f01bd4c2c88913b3f5985c114213ecf3a
salimathritika <126080853+salimathritika@users.noreply.github.com>
2024-10-17 21:14:42 +0530

    Update main.py

--- DNA_Cryptography/main.py
+++ DNA_Cryptography/main.py
@@ -1,5 +1,20 @@
-# main.py
-import gui
+#for testing purposes
 
-if __name__ == "__main__":
-    gui.root.mainloop()
+from dna_encoding import encode_to_dna, decode_from_dna, encoding_table, reverse_table
+
+
+def encrypt_text(pt):
+    encrypted_dna = encode_to_dna(pt, encoding_table)
+    return encrypted_dna
+
+def decrypt_dna(ct):
+    decrypted_dna=decode_from_dna(ct,reverse_table)
+    return decrypted_dna
+
+pt=input("Enter plaintext:")
+ct=encrypt_text(pt)
+print("Encrypted message:",ct)
+ct=input("Enter the DNA to be decrypted:")
+dec=decrypt_dna(ct)
+print("Decrypted DNA:",dec)
+
