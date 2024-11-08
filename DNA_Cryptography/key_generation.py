54f5b37da436bde4db63f3d31e5200063b5ba3d6
salimathritika <126080853+salimathritika@users.noreply.github.com>
2024-11-08 18:50:44 +0530

    Delete DNA_Cryptography directory

--- DNA_Cryptography/key_generation.py
+++
@@ -1,6 +0,0 @@
-import random
-
-# Generate a dynamic key based on the length of the DNA sequence
-def generate_dynamic_key(length):
-    bases = ['A', 'T', 'C', 'G']
-    return ''.join(random.choices(bases, k=length))
